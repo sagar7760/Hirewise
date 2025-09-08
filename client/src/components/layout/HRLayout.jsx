49df221b577a75849dff9055046fdef4fa3a841c
Sagar Soradi <sagarsordi011@gmail.com>
2025-09-09 01:56:39 +0530

    updates in HR dashboard

--- client/src/components/layout/HRLayout.jsx
+++ client/src/components/layout/HRLayout.jsx
@@ -14,7 +14,7 @@ const HRLayout = ({ children }) => {
         </main>
         
         {/* Footer */}
-        <div className="bg-white border-t border-gray-200 px-4 py-3">
+        <div className="bg-white border-t border-gray-200 px-4 py-1">
           <div className="text-center">
             <p className="text-sm text-gray-500 font-['Roboto']">
               © 2025 HireWise. All rights reserved.
